bc4f72f59565b734bddcdf7005925de03e467fbe
Andreas Galanis <agalanis7@gmail.com>
2019-08-08 15:31:15 -0400

    changed to pg db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#User.create!([
+#  {email: "agalanis7@gmail.com", encrypted_password: "$2a$11$UU2BP7vvZ0RSjDXWmQscNugsGMwG2wzbXnxCholXfCoeN3PL0/5q2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: true},
+#  {email: "user@user.com", encrypted_password: "$2a$11$H2CUffkMucHP8uKmj4Mlhu3N1ofLDtmBEzJyRx61E0B8zOZB4YkL.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: false},
+#  {email: "leop@leo.com", encrypted_password: "$2a$11$kfukjuU2UEeKnEZjazQAmer2uJgPuj0fib9bEEYw.oGdKQ4QmZ6a6", reset_password_token: nil, reset_password_sent_at: "2019-07-30 14:46:00", remember_created_at: nil, admin: false},
+#  {email: "admin@admin.com", encrypted_password: "$2a$11$3KlEhszNgMO6aXjXUV1RT.gDTFN.8cfzUKPNvU2srQWGRwxWECGWS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: true}
+#])
+Alcohol.create!([
+  {title: "Patron XO Cafe ", quantity: "1.0", description: "750ml "},
+  {title: "Bacardi Superior", quantity: "1.4", description: "1L"},
+  {title: "Captain Morgan", quantity: "1.2", description: "1L"},
+  {title: "Sauza Silver Tequila", quantity: "1.95", description: "1L"},
+  {title: "Hiram Walker Sambuca ", quantity: "1.0", description: "750ml"},
+  {title: "Gallo Extra Dry Vermouth", quantity: "0.15", description: "750ml"},
+  {title: "Gallo Sweet Vermouth", quantity: "0.3", description: "750mL"},
+  {title: "Ancho Reyes Chile Liqueur", quantity: "0.15", description: "750ml"},
+  {title: "Malibu Rum", quantity: "0.4", description: "1L"},
+  {title: "St. Germain", quantity: "0.15", description: "750ml"},
+  {title: "Bulleit Rye ", quantity: "0.9", description: "1L"},
+  {title: "Jim Beam", quantity: "1.3", description: "1L"},
+  {title: "Dewar's White Label", quantity: "0.9", description: "1L"},
+  {title: "Jim Beam Fire", quantity: "0.6", description: "1L"},
+  {title: "Sauza Reposado Tequila", quantity: "1.75", description: "1L"},
+  {title: "Tito's Vodka ", quantity: "1.3", description: "1L"},
+  {title: "Absolut Vodka", quantity: "0.9", description: "1L"},
+  {title: "Tanqueray Gin", quantity: "0.95", description: "1L"},
+  {title: "Bombay Sapphire Gin", quantity: "0.5", description: "1L"},
+  {title: "Aristocrat Vodka", quantity: "0.3", description: "1L"},
+  {title: "Well Made Gin", quantity: "0.4", description: "1L"},
+  {title: "Ron Llave Rum", quantity: "0.5", description: "1L"},
+  {title: "Arandas Plata Tequila", quantity: "0.15", description: "750mL"},
+  {title: "DeKuyper Triple Sec", quantity: "0.75", description: "1L"},
+  {title: "Kentucky Deluxe Whiskey", quantity: "0.8", description: "1L"},
+  {title: "DeKuyper Peach Schnapps", quantity: "0.5", description: "1L"},
+  {title: "DuBouchett Melon Liquer", quantity: "0.8", description: "1L"},
+  {title: "DuBouchett Butterscotch Schnapps", quantity: "0.15", description: "1L"},
+  {title: "Patagonia", quantity: "38.0", description: "Beer"},
+  {title: "Presidente", quantity: "26.0", description: "Beer"},
+  {title: "Modelo", quantity: "19.0", description: "Beer"},
+  {title: "Corona", quantity: "32.0", description: "Beer"},
+  {title: "Corona Light", quantity: "3.0", description: "Beer"},
+  {title: "Lagunitas", quantity: "31.0", description: "IPA"},
+  {title: "Goose Island", quantity: "26.0", description: "IPA"},
+  {title: "Guinness", quantity: "21.0", description: "Beer"},
+  {title: "Guinness Draught", quantity: "3.0", description: "Beer"},
+  {title: "Yuengling", quantity: "75.0", description: "Beer"},
+  {title: "Shock Top", quantity: "18.0", description: "Beer"},
+  {title: "Stella Artois", quantity: "23.0", description: "Beer"},
+  {title: "Becks", quantity: "10.0", description: "Beer"},
+  {title: "Heineken", quantity: "24.0", description: "Beer"},
+  {title: "Estrella Jalisco", quantity: "2.0", description: "Beer"},
+  {title: "Mondavi Chardonnay", quantity: "3.3", description: "Chardonnay"},
+  {title: "Pabst Blue Ribbon", quantity: "25.0", description: "Beer"},
+  {title: "Rolling Rock", quantity: "1.0", description: "Beer"}
+])
